17671476ce462bc63349e74733eca21313825394
Miguel PS <miguel.pari@gmail.com>
2017-09-30 21:00:09 -0300

    pycv-proj-test.py: minimal skeleton suggested

---
+++ pycv-proj-test.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+
+# Developed and tested with (fill in):
+#
+# os:             _?
+# python version: _?
+# opencv version: _?
+
+# Notes:
+#
+#    Write here your notes which are important
+#  ...
+#
+#
+# Use: something like one of
+#    python pycv-proj-test.py images/image_filename
+#    python3 pycv-proj-test.py images/image_filename
+
+...
+import numpy as np
+import cv2
+...
+
+.
+.
+.
+
+if __name__=="__main__":
+    .
+    .
+    .
+
